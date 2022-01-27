8c5948cee7a10ede7eae1a69f354ac7ea5ae12d2
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-01-28 00:49:53 +0330

    Initial Commit

---
+++ TwitterSplashAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSplashAnimation
+//
+//  Created by Ali Aghamirbabaei on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSplashAnimation/TwitterSplashAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSplashAnimationApp.swift
+//  TwitterSplashAnimation
+//
+//  Created by Ali Aghamirbabaei on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSplashAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
